a9dfc7ee5450f2ba863d7cd86cbee40aa3ccb01a
Yakub289 <Yakubsuleiman24@gmail.com>
2023-04-26 13:52:51 -0700

    Prototype

--- main.h
+++ main.h
@@ -175,10 +175,10 @@ int set_ali(var_s *, char *);
 int print_ali(list_s *node);
 
 /* aux_error_1.c */
-int _ err_ato(char *c);
+int _err_ato(char *c);
 void get_error(var_s *dsp, char *estr);
 int get_di(int input, int fd);
 char *trans_numbe(long int num, int base, int flags);
-void delete_ comment(char *buffer);
+void delete_comment(char *buffer);
 
 #endif
