103d6d2298e08acfeb7797ca3212e3cc38db58f0
lyka-ed <lykaedem11@gmail.com>
2023-04-26 10:18:43 -0700

    Auxillary strings

--- aux_string_1.c
+++ aux_string_1.c
@@ -5,29 +5,31 @@
 #include <string.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - Function that will print length of a string character.
+ * @s: string length.
  *
- * Return: integer length of string
+ * Return: integer length of string.
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int x = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		x++;
+
+	return (x);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - Function that will print the comparison of two string character..
+ * @s1: string one.
+ * @s2: string two.
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return: if s1 < s2 return -1 or if s1 > s2 return 1
+ * else if s1 == s2 return 0.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -38,6 +40,7 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
+
 	if (*s1 == *s2)
 		return (0);
 	else
@@ -45,35 +48,38 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * begin_at - Function that will checks if pin starts with hays.
+ * @hay_s: string character.
+ * @pin: substring.
  *
- * Return: address of next char of haystack or NULL
+ * Return: address to access char or Nothing 0.
  */
-char *starts_with(const char *haystack, const char *needle)
+char *begin_at(const char *hay_s, const char *pin)
 {
-	while (*needle)
-		if (*needle++ != *haystack++)
+	while (*pin)
+		if (*pin++ != *hay_s++)
 			return (NULL);
-	return ((char *)haystack);
+
+	return ((char *)hay_s);
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * _strcat - Function to prints a concatenated two string character.
+ * @dist: destination of string.
+ * @rsc: source of string.
  *
- * Return: pointer to destination buffer
+ * Return: pointer to access destination buffer.
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dist, char *rsc)
 {
-	char *ret = dest;
+	char *ate = dist;
 
-	while (*dest)
-		dest++;
-	while (*src)
-		*dest++ = *src++;
-	*dest = *src;
-	return (ret);
+	while (*dist)
+		dist++;
+	while (*rsc)
+		*dist++ = *rsc++;
+
+	*dist = *rsc;
+
+	return (ate);
 }
